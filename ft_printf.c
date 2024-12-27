c81490b6455bd4555d3857435d5466922c83a872
christian_reynaltt <creynaltt@maristak.net>
2024-12-27 17:53:05 +0100

    Feat:
    - *str == 'c' function added
    - *str == 's' function added

--- ft_printf.c
+++ ft_printf.c
@@ -1,163 +1,69 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: creynalt <creynalt@student.42urduliz.co    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/20 17:04:20 by creynalt          #+#    #+#             */
-/*   Updated: 2024/12/20 20:30:21 by creynalt         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libftprintf.h"
 
 #include <stdio.h>
-#include <unistd.h>
+#include <stdarg.h>
 
-void ft_putchar(char c) {
+void ft_putchar(char c, size_t *printed_chars_counter)
+{
     write(1, &c, 1);
+    (*printed_chars_counter)++;
 }
 
-void ft_putstr(char *str) {
-    while (*str) {
-        ft_putchar(*str);
-        str++;
+void ft_putstr(char *c, size_t *printed_chars_counter)
+{
+    if (!c)
+        c = "(null)";
+    while (*c)
+    {
+        ft_putchar(*c, printed_chars_counter);
+        c++;
     }
 }
 
-void ft_putnbr(int n) {
-    if (n == -2147483648) {
-        ft_putstr("-2147483648");
-        return;
-    }
-    if (n < 0) {
-        ft_putchar('-');
-        n = -n;
-    }
-    if (n >= 10) {
-        ft_putnbr(n / 10);
-    }
-    ft_putchar(n % 10 + '0');
-}
-
-void ft_selector(char chr, va_list vargs) {
-    if (chr == 'c') {
-        char c = va_arg(vargs, int);
-        ft_putchar(c);
-    } else if (chr == 's') {
-        char *str = va_arg(vargs, char*);
-        ft_putstr(str);
-    } else if (chr == 'd' || chr == 'i') {
-        int num = va_arg(vargs, int);
-        ft_putnbr(num);
-    } else if (chr == '%') {
-        ft_putchar('%');
-    }
+void ft_selector(char *str, size_t *printed_chars_counter, va_list vargs)
+{
+    if (*str == 'c')
+        ft_putchar(va_arg(vargs, int), printed_chars_counter);
+    else if (*str == 's')
+        ft_putstr(va_arg(vargs, char*), printed_chars_counter);
 }
 
 int ft_printf(char const *str, ...) {
     va_list vargs;
-    int i = 0;
-    int printed_chars = 0;
+    size_t printed_chars_counter = 0;
 
     if (!str)
         return 0;
 
     va_start(vargs, str);
 
-    while (str[i]) {
-        if (str[i] == '%') {
-            i++;
-            ft_selector(str[i], vargs);
-            printed_chars++;
-        } else {
-            ft_putchar(str[i]);
-            printed_chars++;
-        }
-        i++;
+    while (*str) {
+        if (*str == '%') {
+            str++;
+            ft_selector((char *)str, &printed_chars_counter, vargs);
+        } else 
+            ft_putchar(*str, &printed_chars_counter);
+        str++;
     }
 
     va_end(vargs);
-    return printed_chars;
+    return printed_chars_counter;
 }
 
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 42;
-    unsigned int un = -42;
-    char c = '\0';
-    char *s = NULL;
-    char *ptr = "Test";
-    int rlen1, rlen2 = 0;
-    // Test 1: Mix
-    rlen1 = ft_printf("\033[0;33mTest Mix: %c %s %d %i %u %x %X %p %%\n\033[0m", c, s, n, n, un, n, n, s);
-    rlen2 = printf("\033[0;32mTest Mix: %c %s %d %i %u %x %X %p %%\n\033[0m", c, s, n, n, un, n, n, s);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 2: Char
-    rlen1 = ft_printf("\033[0;33mTest char: %c\n\033[0m", 'A');
-    rlen2 = printf("\033[0;32mTest char: %c\n\033[0m", 'A');
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 3: String
-    rlen1 = ft_printf("\033[0;33mTest string: %s\n\033[0m", "mundo");
-    rlen2 = printf("\033[0;32mTest string: %s\n\033[0m", "mundo");
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 4: Hex minusculas
-    rlen1 = ft_printf("\033[0;33mTest hex: %x\n\033[0m", 25555555);
-    rlen2 = printf("\033[0;32mTest hex: %x\n\033[0m", 25555555);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 5: Puntero
-    rlen1 = ft_printf("\033[0;33mTest ptr: %p\n\033[0m", ptr);
-    rlen2 = printf("\033[0;32mTest ptr: %p\n\033[0m", ptr);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 6: Hex mayusculas
-    rlen1 = ft_printf("\033[0;33mTest HEX: %X\n\033[0m", 255);
-    rlen2 = printf("\033[0;32mTest HEX: %X\n\033[0m", 255);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 7: Decimal
-    rlen1 = ft_printf("\033[0;33mTest dec: %d\n\033[0m", 777);
-    rlen2 = printf("\033[0;32mTest dec: %d\n\033[0m", 777);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 8: Int
-    rlen1 = ft_printf("\033[0;33mTest int: %i\n\033[0m", 777);
-    rlen2 = printf("\033[0;32mTest int: %i\n\033[0m", 777);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 9: Unsigned int
-    rlen1 = ft_printf("\033[0;33mTest uns: %u\n\033[0m", -0);
-    rlen2 = printf("\033[0;32mTest uns: %u\n\033[0m", -0);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 10: Porcentaje
-    rlen1 = ft_printf("\033[0;33mTest porcentaje: %%\n\033[0m");
-    rlen2 = printf("\033[0;32mTest porcentaje: %%\n\033[0m");
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
-    // Test 11: Locura
-    rlen1 = ft_printf("\033[0;33mTest locura: %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%\n\033[0m", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
-    rlen2 = printf   ("\033[0;32mtest locura: %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%\n\033[0m", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
-    printf("\033[0;33mft_printf devolvió: %d\n\033[0m", rlen1);
-    printf("\033[0;32mprintf devolvió: %d\n\033[0m", rlen2);
-    printf("\n\n");
+    int len1, len2;
+
+    len1 = ft_printf("Hola %s, tu edad es %d y tu inicial es %c.\n", "Carlos", 25, 'C');
+    len2 = printf("Hola %s, tu edad es %d y tu inicial es %c.\n", "Carlos", 25, 'C');
+    ft_printf("Mi printf: %d caracteres.\n", len1);
+    printf("Printf real: %d caracteres.\n", len2);
+
+    ft_printf("Un número hexadecimal: %x, %X\n", 255, 255);
+    ft_printf("Un puntero: %p\n", main);
+    ft_printf("Un número sin signo: %u\n", 4294967295u);
+    ft_printf("Porcentaje: %%\n");
     return 0;
 }
 
